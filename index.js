bca0e0f572a6deeab28b8a073617d6f46f9d8384
Eugene <eugeneayonga@gmail.com>
2022-06-10 17:06:29 +0300

    commit tree d60244aa2efd875f08a52ba691373b44b44b9147

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+console.log(multiply);
+
+
+// MULTIPLYING ABOVE BY ARROW FUNCTIONS
+/*const multiply = (num1, num2) => num1 * num2;
+console.log(multiply(2, 31));*/
+
+
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+/*const random = (num3) => Math.random(num3.isInteger !== 0);
+console.log(random);*/
